77e1b552623d16303060434041a4f48d0bceb054
veggiepilot <johanneschitura@gmail.com>
2021-12-21 07:50:55 -0500

    Added update functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,22 +22,45 @@ export const FeedbackProvider = (({children}) => {
         }
     ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
 
+    //Delete feedback
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id ))
         }     
     }
-
+    //Add Feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
 
+    //Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
+
+    //Update feedback item
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => item.id === id ? {
+            ...item, ...updItem
+        } : item))
+    }
+
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
-        addFeedback
+        feedbackEdit,
+        addFeedback,
+        editFeedback,
+        updateFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
