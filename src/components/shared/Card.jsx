6a15c82a78c3b865c5df223c1a33049e6992b18d
veggiepilot <johanneschitura@gmail.com>
2022-02-26 23:00:52 -0500

    Added concurrently and j-son server dependencies

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,12 +1,6 @@
 import PropTypes from 'prop-types'
 
 function Card({ children, reverse }) {
-    // return (
-    //     <div className={`card ${reverse && 'reverse'}`}>
-    //         {children}
-    //     </div>
-    // )
-
     return (
         <div className="card" style={{
             backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#ffff', 
